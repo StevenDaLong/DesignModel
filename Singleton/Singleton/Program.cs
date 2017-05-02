0d3a3db21c4f3dc3c2298cc1db5eaa30646bf7f2
steven <shiyanlong_2005@126.com>
2017-05-02 20:35:03 +0800

    singelton 例子更新

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //单例模式（多线程）
+            Singleton.Instance.Say();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Singleton/Singleton/Singleton.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+
+        private static readonly object locker = new object();
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    //多线程lock锁 确保单例
+                    lock (locker)
+                    {
+                        return new Singleton();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        private Singleton()
+        {
+        }
+
+        public void Say()
+        {
+            Console.WriteLine("Im singleton !");
+        }
+    }
+}
